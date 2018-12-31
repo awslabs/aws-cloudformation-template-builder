dde2717591828b32c2a182718183e91e12f40bf4
Steve Engledow <sengledo@amazon.co.uk>
2018-12-31 15:36:14 +0000

    Add some initial thoughts

---
+++ spec.go
@@ -0,0 +1,13 @@
+package spec
+
+type Spec struct {
+	Version       string
+	PropertyTypes map[string]PropertySpec
+	ResourceTypes map[string]ResourceSpec
+}
+
+type PropertySpec struct {
+}
+
+type ResourceSpec struct {
+}
