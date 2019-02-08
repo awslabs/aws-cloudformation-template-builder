33e053ad4e2b2d315a8c39d4ffb09d696f982d39
Connor Kirkpatrick <ckp@amazon.com>
2019-02-08 16:14:42 +0000

    Finish generate functionality

--- generate/main.go
+++ generate/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bytes"
 	"codecommit/builders/cfn-skeleton/spec"
 	"errors"
 	"fmt"
@@ -27,87 +28,104 @@ var (
 	propertyFuncs map[string]PropertyFunc
 )
 
+// Filename of the templates used
 const (
 	resourceTemplate = "resource.tmpl"
 	propertyTemplate = "property.tmpl"
 )
 
 func main() {
-	// Make file of example resource
-	// Needs to be fleshed out to iterate over all resources and properties in the spec.
-	// Each resource and property needs to be registered with it's respective map
-	// either resourceFuncs or propertyFuncs.
+
 	resources := spec.Cfn.ResourceTypes
-	//properties := spec.Cfn.PropertyTypes
+	properties := spec.Cfn.PropertyTypes
 
 	// Make resourceFuncs
-
-	for name := range resources {
-		generateResource(name)
+	for rt := range resources {
+		generateResource(rt)
 
 	}
 
 	// Make propertyFuncs
-	// This won't work
-	// propertyFunc not yet implemented.
-
-	// for name, _ := range properties {
-	// 	generateProperty(name)
-	// }
+	for pt := range properties {
+		generateProperty(pt)
+	}
 
 }
 
-func generateResource(name string) {
+// generateResource creates a file containing a
+// function that returns a resourceType
+func generateResource(resourceType string) {
 
+	name := nameFromAWSType(resourceType)
 	// get the resource/property type
-	resource, err := getResourceType(name)
+	resource, err := getResourceType(resourceType)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := build(name, resourceTemplate, resource)
 	if err != nil {
 		panic(err)
 	}
 
 	// Write the file
-	err = writeResourceFile(name, resource)
+	err = writeFile(name, b)
 	if err != nil {
 		panic(err)
 	}
 }
 
-// getResourceType returns a ResourceType for a given name. If it
-// cannot find the type, it will return an error and an empty
-// spec.ResourceType
-func getResourceType(name string) (spec.ResourceType, error) {
-	resource, ok := spec.Cfn.ResourceTypes[name]
-	if !ok {
-		return spec.ResourceType{}, errors.New("Cannot resolve resource name: " + name)
-	}
-	return resource, nil
-}
-
-// writeResourceFile takes a qualified resource name
-// (eg AWS::S3::Bucket) and
-// generates a file containin ResourceFunc and
-// PropertyFunc
-func writeResourceFile(name string, rt spec.ResourceType) error {
-
-	cleanedName := nameFromAWSType(name)
-
-	out, err := os.Create("../spec/types/" + cleanedName + ".go")
-	defer out.Close()
+// generateProperty creates a file containing a
+// function that returns a propertyType
+func generateProperty(propertyType string) {
+	name := nameFromAWSType(propertyType)
+	property, err := getPropertyType(propertyType)
 	if err != nil {
 		panic(err)
 	}
 
-	t := template.Must(template.ParseFiles(resourceTemplate))
-	err = t.ExecuteTemplate(out, "template", struct {
-		Name         string
-		RTDefinition interface{}
+	b, err := build(name, propertyTemplate, property)
+	if err != nil {
+		panic(err)
+	}
+
+	err = writeFile(name, b)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// build takes a name, templateName and a resourceType or propertyType
+// it executes the template into a buffer and returns the array of bytes
+func build(name string, templateName string, input interface{}) ([]byte, error) {
+	var b bytes.Buffer
+	t := template.Must(template.ParseFiles(templateName))
+	err := t.ExecuteTemplate(&b, "template", struct {
+		Name      string      // Name of the function to be created
+		ReturnVal interface{} // Body of the function
 	}{
-		Name: cleanedName,
+		Name: name,
 
 		// Use %#v directive to output the entire struct in parseable format
-		RTDefinition: fmt.Sprintf("%#v", rt),
+		ReturnVal: fmt.Sprintf("%#v", input),
 	})
 
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+// writeFile writes the provided byte sequence to a file of the provided name
+func writeFile(name string, b []byte) error {
+	out, err := os.Create("../spec/types/" + name + ".go")
+	defer out.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = out.Write(b)
 	if err != nil {
 		return err
 	}
@@ -123,4 +141,26 @@ func nameFromAWSType(name string) string {
 	temp := strings.Replace(name, "::", "", -1)
 	temp = strings.Replace(temp, ".", "_", -1)
 	return temp
+}
+
+// getResourceType returns a ResourceType for a given name. If it
+// cannot find the type, it will return an error and an empty
+// spec.ResourceType
+func getResourceType(name string) (spec.ResourceType, error) {
+	resource, ok := spec.Cfn.ResourceTypes[name]
+	if !ok {
+		return spec.ResourceType{}, errors.New("Cannot resolve resource name: " + name)
+	}
+	return resource, nil
+}
+
+// getPropertyType returns a PropertyType for a given name.
+// If it cannot find the type, it will return an error and an empty
+// spec.PropertyType
+func getPropertyType(name string) (spec.PropertyType, error) {
+	property, ok := spec.Cfn.PropertyTypes[name]
+	if !ok {
+		return spec.PropertyType{}, errors.New("Cannot resolve property name: " + name)
+	}
+	return property, nil
 }
